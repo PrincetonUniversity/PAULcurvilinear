674707ec39144e35f700c4ff850bdfd15e59b02c
wansyuwai <shuhuiy@princeton.edu>
2020-03-03 10:56:52 -0500

    Update wrapTo0halfPi.m
    correct function

--- wrapTo0halfPi.m
+++ wrapTo0halfPi.m
@@ -1,10 +1,10 @@
 function output=wrapTo0halfPi(angleInRad)
 % the input needs to be an angle in [0, pi]
 angleInRad=wrapTo0Pi(angleInRad);
-coeff=sign(0.5.*pi-angleInRad);
-output=((1-coeff)/2).*pi+coeff.*angleInRad;
+coeff=((0.5.*pi-angleInRad)>=0);
+output=(1-coeff).*pi+(coeff.*2-1).*angleInRad;
 
 function output=wrapTo0Pi(angleInRad)
 
 temp = wrapToPi(angleInRad);
-output = temp+pi*((1-sign(temp))/2);
+output = temp+pi*(temp<0);
