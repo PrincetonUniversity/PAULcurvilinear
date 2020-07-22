f84cabf6f1f33c84768e15bc3c6b67fb32ec7f7f
wansyuwai <shuhuiy@princeton.edu>
2020-07-22 10:17:35 -0400

    delete test

--- DistanceBtPoints.m
+++
@@ -1,9 +0,0 @@
-function dists=DistanceBtPoints(Pts1,Pts2)
-% distance between each pair of points
-% Pt1 and Pt2 are nx2 or nx3 matrices    
-if size(Pts1,2) == 2
-    dists = sqrt((Pts1(:,1)-Pts2(:,1)).^2+(Pts1(:,2)-Pts2(:,2)).^2);
-elseif size(Pts1,2) == 3
-    dists = sqrt((Pts1(:,1)-Pts2(:,1)).^2+(Pts1(:,2)-Pts2(:,2)).^2+(Pts1(:,3)-Pts2(:,3)).^2);
-end
-        
--- imbinarize2.m
+++
@@ -1,7 +0,0 @@
-function output = imbinarize2(image)
-% adapted version of imbinarize to get rid of all-one image outputs
-if graythresh(image)==0
-    output = false(size(image));
-else
-    output = imbinarize(image);
-end
